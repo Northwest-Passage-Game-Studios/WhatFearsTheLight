2c52f624b8fe3732683655c590d89d985915db74
TheAmericanEmu <harringtondingc@gmail.com>
2026-03-22 16:40:54 -0600

    Add Wendigo monster and flashlight spook
    Introduce the Wendigo enemy and enable the flashlight to spook it.
    
    - Renamed placeholderEnemy.tscn -> Monsters/Wendigo/Wendigo.tscn, converted root to CharacterBody3D, added Box CollisionShape, and adjusted subnodes/transforms.
    - Updated Monsters/Wendigo/wendigo.gd: extend CharacterBody3D, use NavigationAgent3D movement, set velocity + move_and_slide, add look helper and spooked state, and refine spook/timer logic.
    - Add Convex RayCone.tres resource and Debug/light_baker.tscn (baked mesh + collision) for flashlight cone geometry.
    - Add ShapeCast3D to player_view_model.tscn (uses RayCone) and tweak flashlight node transform and autoplay animation.
    - Update Objects/Items/Flashlights/flash_light.gd to check ShapeCast3D collisions and call spook() on detected Wendigo instances.
    - Update worldAssets/WorldSpaces/monster_debug.tscn to reference the new Wendigo scene and adjust player placement; include a .tmp helper file.
    - Add physics layer name for layer 2 as "Monsters" in project.godot.
    
    These changes integrate the new Wendigo enemy into the scene, connect the player flashlight detection to spooking behavior, and add supporting resources for the flashlight cone and debug scene.

--- Monsters/Wendigo/wendigo.gd
+++ Monsters/Wendigo/wendigo.gd
@@ -1,16 +1,21 @@
-class_name wendigo extends Node3D
+class_name wendigo extends CharacterBody3D
 @onready var kill_timer: Timer = $Kill_Timer
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 
 
 @export var target:Node3D
-@export var move_speed=5
+@export var move_speed:=5
 
 var physics_delta: float
 var is_chasing := false
 var is_red_eye =false
+var is_spooked :=false
 
-
+func Totally_Better_Look_At(target_pos:Vector3):
+	look_at(target_pos)
+	self.rotation.x=0
+	self.rotation.z=0
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,17 +28,19 @@ func _ready() -> void:
 func way_point_reached():
 
 	var next_pos=navigation_agent_3d.get_next_path_position()
-	var new_velocity: Vector3 = global_position.direction_to(next_pos) * move_speed
+	Totally_Better_Look_At(next_pos)
+	var new_velocity: Vector3 = global_position.direction_to(next_pos) * 6
 	if navigation_agent_3d.avoidance_enabled:
 		navigation_agent_3d.set_velocity(new_velocity)
 	else:
 		_on_velocity_computed(new_velocity)
 
 func _on_velocity_computed(safe_velocity: Vector3) -> void:
-	global_position = global_position.move_toward(global_position + safe_velocity, physics_delta * move_speed)
+	velocity=safe_velocity
+	move_and_slide()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(navigation_agent_3d.target_position)
 	if navigation_agent_3d.target_position!=Vector3.ZERO:
 		navigation_agent_3d.target_position=target.position
 	
@@ -42,9 +49,13 @@ func _physics_process(delta: float) -> void:
 	if is_chasing:
 		way_point_reached()
 
-
+	else:
+		Totally_Better_Look_At(target.global_position)
 func _on_kill_timer_timeout() -> void:
 	is_chasing=true
 	
 func spook():
-	kill_timer.stop()
+	if is_chasing==false and kill_timer.is_stopped()==false and is_spooked==false:
+		kill_timer.stop()
+		is_chasing=false
+		is_spooked=true
--- Objects/Items/Flashlights/flash_light.gd
+++ Objects/Items/Flashlights/flash_light.gd
@@ -2,5 +2,16 @@ extends Item
 
 var is_broken:=false
 
+@onready var shape_cast_3d: ShapeCast3D = $ShapeCast3D
+
+
+
+
 func _process(delta: float) -> void:
-	pass
+	if self.visible==true:
+		if shape_cast_3d.is_colliding():
+			var obj :Node3D= shape_cast_3d.get_collider(0)
+
+			if obj is wendigo:
+				print(obj)
+				obj.spook()
