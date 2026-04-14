8ad131b7d16797a38c4111e294f80f3d6517b12d
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-04-14 13:22:28 -0500

    Add boat light flicker script and beach scene updates
    Add a GDScript (12219_boat_v_2l_2.gd) to control a flickering spotlight on the boat, with Timer nodes and signal connections; include a UID file for the script. Update beach_scene.tscn: attach the new script to the boat mesh, add Timer and flickerTimer nodes, connect timeout signals, adjust spotlight range/visibility, move player transform, and add CSG boxes for collision. Introduce a WorldEnvironment with a ProceduralSkyMaterial, FogVolume and FogMaterial for scene fog/ambient settings. Add two new sky EXR assets (mud_road and sunflowers) with import presets and update qwantani_night import settings (compression, mipmaps, and VRAM usage).

---
+++ worldAssets/WorldSpaces/Beach_Level/12219_boat_v_2l_2.gd
@@ -0,0 +1,28 @@
+extends MeshInstance3D
+@onready var spot_light_3d: SpotLight3D = $CSGSphere3D/SpotLight3D
+@onready var csg_sphere_3d: CSGSphere3D = $CSGSphere3D
+var flickering:=false
+@onready var timer: Timer = $Timer
+@onready var flicker_timer: Timer = $flickerTimer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if flickering:
+		csg_sphere_3d.visible=randi_range(0,1)==1
+	else:
+		csg_sphere_3d.visible=true
+
+
+func _on_timer_timeout() -> void:
+	timer.start(randf_range(1.0,7.0))
+	flickering=true
+	flicker_timer.start(randf_range(0.1,0.9))
+
+
+func _on_flicker_timer_timeout() -> void:
+	flickering=false
