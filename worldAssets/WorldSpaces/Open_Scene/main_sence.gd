f9098967efa8a432d32731dafa0d2aac5efad563
TheAmericanEmu <harringtondingc@gmail.com>
2026-04-25 18:33:54 -0600

    Add cloud & smoke shaders, menu and open scenes
    Introduce visual assets and scene logic for clouds and smoke, plus initial UI/entry scenes. Added: Shaders/Clouds/cloud.tres (VisualShader), multiple Shaders/Smoke assets (Mesh.tres, Smoke_Scene.tscn, Stylized_Smoke_Shader.gdshader and related UIDs), and an ArrayMesh surface for smoke. Added worldAssets scripts and scenes for the main menu and opening: main_menu.gd (+ UID), updated worldAssets/WorldSpaces/MainMenu/main_menu.tscn, and new Open_Scene files (JokeOpening.txt, Main_Sence.tscn, main_sence.gd + UID). These additions set up cloud/smoke rendering and the initial main menu/opening scene structure.

---
+++ worldAssets/WorldSpaces/Open_Scene/main_sence.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export_file("*.json", "*.txt") var data_file: String
+@onready var text_label: Label = $CanvasLayer/Control/Text_Label
+
+var texts:Array[String]=[]
+
+func _ready() -> void:
+	var loaded = FileAccess.get_file_as_string(data_file)
+	var packed_texts := loaded.split("\n")
+	for line in packed_texts:
+		texts.append(line)
+	
+	for text in texts:
+		var text_change_tween = create_tween()
+		text_change_tween.tween_property(text_label,"text",text,2)
+		await text_change_tween.finished
+		await get_tree().create_timer(2).timeout
+		
+	get_tree().change_scene_to_file("res://worldAssets/WorldSpaces/Beach_Level/beach_scene.tscn")	
+	
